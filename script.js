6b86b6f591128f4364109428dcaab43868f4ed7b
Russell <russelldev@protonmail.com>
2021-02-03 07:52:36 +0000

    Add heart eventlistener

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const loveMe = document.querySelector('.loveMe');
 const times = document.querySelector('#times');
 
+let clikTime = 0;
+
+loveMe.addEventListener('click', (e) => {
+    if (clikTime === 0) {
+        clikTime = new Date().getTime();
+    } else {
+        if ((new Date().getTime() - clikTime) < 800) {
+            createHeart(e);
+            clikTime = 0;
+        } else {
+            clikTime = new Date().getTime();
+        }
+    }
+});
